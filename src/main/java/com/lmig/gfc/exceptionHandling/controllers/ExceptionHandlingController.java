66f68b6cde93426d5d3ae253aa1f41a96e9e2240
Doug George <douglas.george@libertymutual.com>
2017-11-27 15:44:17 -0500

    This is the exception-handling project

---
+++ src/main/java/com/lmig/gfc/exceptionHandling/controllers/ExceptionHandlingController.java
@@ -0,0 +1,77 @@
+package com.lmig.gfc.exceptionHandling.controllers;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class ExceptionHandlingController {
+	
+	 @GetMapping("/")
+	    public String showForm() {
+	        return "exceptionHandling/default";
+	    }
+
+	    @PostMapping("/handleString")
+	    public ModelAndView handleString(String probablySomeText) {
+	        ModelAndView mv = new ModelAndView("exceptionHandling/default");
+	        try {
+	        	mv.addObject("stringMessage", probablySomeText.substring(0,4));
+	        } catch (Exception e) {
+	        	mv.addObject ("stringResult", "string should be four characters");
+	        }
+	        return mv;
+	    }
+
+	    @PostMapping("/handleUrl")
+	    public ModelAndView handleUrl(String probablyAUrl) throws MalformedURLException {
+	        ModelAndView mv = new ModelAndView("exceptionHandling/default");
+	        try {
+	        mv.addObject("urlResult", new URL(probablyAUrl));
+	        } catch (Exception e) {
+	        	mv.addObject("urlFailure", "not a valid URL");
+	        }
+	        return mv;
+	    }
+
+	    @PostMapping("/handleInteger")
+	    public ModelAndView handleInteger(String probablyAnInteger) {
+	        ModelAndView mv = new ModelAndView("exceptionHandling/default");
+	        try {
+	        mv.addObject("integerResult", Integer.parseInt(probablyAnInteger));
+	        } catch (Exception e) {
+	        	mv.addObject("integerResult", "not an integer");
+	        }
+	        return mv;
+	    }
+
+	    @PostMapping("/handleDecimal")
+	    public ModelAndView handleDecimal(String probablyADecimal) {
+	        ModelAndView mv = new ModelAndView("exceptionHandling/default");
+	        try {
+	        mv.addObject("decimalResult", Double.parseDouble(probablyADecimal));
+	        } catch (Exception e) {
+	        	mv.addObject("decimalResult","not a decimal");
+	        }
+	        return mv;
+	    }
+
+	    @PostMapping("/handleDate")
+	    public ModelAndView handleDate(String probablyADate) {
+	        ModelAndView mv = new ModelAndView("exceptionHandling/default");
+	        try {
+	        mv.addObject("dateResult", LocalDate.parse(probablyADate));
+	        } catch (Exception e) {
+	        	mv.addObject("dateResult", "not a date");
+	        }
+	        return mv;
+	    }
+
+	}
